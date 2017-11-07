fea3add2ade1d064d590d897459b2f3eae850190
Louis t.b <louistb@me.com>
2017-11-07 15:25:07 -0500

    Cam Stiwch Script Added

---
+++ Assets/Scripts/GUI/camSwitchController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camSwitchController : MonoBehaviour {
+
+	public void Start() {
+		switchCam("Ruche");
+	}
+
+	//ALL OFF
+	public void switchCam(string toActivate) {
+		var views = GameObject.FindGameObjectsWithTag("aView");
+		foreach (var view in views) {
+			view.SetActive(false);
+		}
+	//SELECTED ON
+		GameObject.Find (toActivate).SetActive (true);
+	}
+
+}
