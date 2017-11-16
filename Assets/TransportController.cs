c42255f829ce7fb4a0b6a7cc3d66be1247aa473c
Louis t.b <louistb@me.com>
2017-11-16 14:34:30 -0500

    New Transport/TimeDisplay

---
+++ Assets/TransportController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class TransportController : MonoBehaviour {
+
+    public Text timUi;
+	// Update is called once per frame
+	void Update () {
+        timUi.text = System.DateTime.Now.ToString();
+    }
+}
