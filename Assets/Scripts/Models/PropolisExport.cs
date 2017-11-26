b790972ae48c31f86e7a349010fea46dcb6ab849
Jean-Christophe Leclerc-Massé <jc@proxee.com>
2017-11-25 19:51:21 -0500

    added battery to propolis export

--- Assets/Scripts/Models/PropolisExport.cs
+++ Assets/Scripts/Models/PropolisExport.cs
@@ -10,6 +10,7 @@ namespace Propolis
     {
 
         public OSC SoundMaxOsc;
+        public OSC BatteryOsc;
 
 
     // Use this for initialization
@@ -26,13 +27,21 @@ namespace Propolis
 
         public void ExportFromModel()
         {
-            if (Propolis.PropolisData.Instance.IsGamePlaying && PropolisData.Instance.LastEvent.Action == PropolisActions.UpdateItemStatus)
+            if (Propolis.PropolisData.Instance.IsGamePlaying)
             {
-                PropolisLastEventState lastEvent = Propolis.PropolisData.Instance.LastEvent;
-                SendOscMessage(
-                    "/"+ lastEvent.Type, 
-                    lastEvent.GroupID,lastEvent.ID, 
-                    PropolisData.Instance.GetItemDataById(lastEvent.GroupID, lastEvent.ID, lastEvent.Type).Status);
+                if (PropolisData.Instance.LastEvent.Action== PropolisActions.UpdateItemStatus)
+                {
+                    PropolisLastEventState lastEvent = Propolis.PropolisData.Instance.LastEvent;
+                    SendOscMessage(
+                        "/" + lastEvent.Type,
+                        lastEvent.GroupID, lastEvent.ID,
+                        PropolisData.Instance.GetItemDataById(lastEvent.GroupID, lastEvent.ID, lastEvent.Type).Status);
+                }
+                else if (PropolisData.Instance.LastEvent.Action == PropolisActions.SetBatteryLevel)
+                {
+                    SendMessage("/battery",PropolisData.Instance.BatteryLevel);
+                }
+
             }
         }
 
@@ -46,6 +55,16 @@ namespace Propolis
             message.values.Add(value2);
             message.values.Add(value3);
             SoundMaxOsc.Send(message);
+        }
+
+        private void SendOscMessage(string address, int value)
+        {
+
+            OscMessage message = new OscMessage();
+
+            message.address = address;
+            message.values.Add(value);
+            BatteryOsc.Send(message);
         }
     }
 
