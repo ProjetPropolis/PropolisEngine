1b0be9541e98f16dde62f4d37d8c801b99cc6499
ProjetPropolis <jclm1992@gmail.com>
2018-01-28 11:36:24 -0500

    debug dummy hive

---
+++ Assets/Scripts/Helpers/EditorDummyVisibility.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorDummyVisibility : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.SetActive(Debug.isDebugBuild);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
