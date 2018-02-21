7709299a339f1fbb9e965cabf4ef15198794bc74
ProjetPropolis <jclm1992@gmail.com>
2018-02-21 12:47:49 -0500

    map control now disabled after play

---
+++ Assets/Scripts/GUI/VisibilityToggle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Propolis;
+
+public class VisibilityToggle : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.SetActive(!PropolisData.Instance.IsGamePlaying);
+	}
+}
